3ef7d5597f1587381d5aebb531547e642144fcc4
Craig Russell <craig@craig-russell.co.uk>
2013-07-05 16:30:24 +0100

    Added basic user properties and tests

--- models/user.py
+++ models/user.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
+#
+# A user class
+#
 
-class User():
+from google.appengine.ext import db
 
-    def __init__(self, name):
-        self.name = name
+class User(db.Model):
+
+    # Properties
+    google_id           = db.StringProperty()
+    google_email        = db.StringProperty()
+    google_name         = db.StringProperty()
+    profile_slug        = db.StringProperty()
+    profile_name        = db.StringProperty()
+    profile_email       = db.StringProperty()
+    profile_web_address = db.StringProperty()
+    created_at          = db.StringProperty()
+    modified_at         = db.StringProperty()
+    credentials         = db.StringProperty()
