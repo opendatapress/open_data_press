b4244100ad0e64f1b40379ee604efb5b2c352774
Craig Russell <craig@craig-russell.co.uk>
2013-07-31 16:53:33 +0100

    Test that API returns valid json obects

--- tests/test_routes/test_api.py
+++ tests/test_routes/test_api.py
@@ -2,6 +2,7 @@
 
 import unittest
 import webapp2
+import simplejson as json
 import main # The app
 
 class TestDashboardHandler(unittest.TestCase):
@@ -11,12 +12,14 @@ class TestDashboardHandler(unittest.TestCase):
         response = main.app.get_response('/api')
         self.assertEqual(response.status_int, 404)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
 
     def test_api_0_path_responds(self):
         response = main.app.get_response('/api/0')
         self.assertEqual(response.status_int, 404)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
 
     def test_api_0_user_path_responds(self):
@@ -27,73 +30,90 @@ class TestDashboardHandler(unittest.TestCase):
         response = main.app.get_response('/api/0/user', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
 
     def test_api_0_data_source_path_responds(self):
         response = main.app.get_response('/api/0/data_source')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source',method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1', method='DELETE')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
 
     def test_api_0_data_view_path_responds(self):
         response = main.app.get_response('/api/0/data_source/1/view')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1/view', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1/view/0')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1/view/0', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/data_source/1/view/0', method='DELETE')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
 
     def test_api_0_google_sheets_path_responds(self):
         response = main.app.get_response('/api/0/google/sheets')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/google/sheets', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/google/sheets/0')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/google/sheets/0', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/google/sheets/0/0')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
 
         response = main.app.get_response('/api/0/google/sheets/0/0', method='POST')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'application/json')
+        self.assertIsInstance(json.loads(response.body), dict)
