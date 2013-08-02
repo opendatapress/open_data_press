5aacbc333fbfbaec782693a19c0328431cd354f5
Craig Russell <craig@craig-russell.co.uk>
2013-08-02 13:33:32 +0100

    Added more useful information to error logging

--- controllers/root.py
+++ controllers/root.py
@@ -15,12 +15,14 @@ class HomeRoute(RequestHandler):
 
 
 def error_404(request, response, exception):
-    logging.error("%s - %s" % (request.path, exception))
+    msg_info = (request.method, request.path_url, request.POST.items(), exception)
+    logging.error("%s %s %s 404 '%s'" % msg_info)
     response.write(static('404.html'))
     response.set_status(404)
 
 
 def error_500(request, response, exception):
-    logging.error("%s - %s" % (request.path, exception))
+    msg_info = (request.method, request.path_url, request.POST.items(), exception)
+    logging.error("%s %s %s 500 '%s'" % msg_info)
     response.write(static('500.html'))
     response.set_status(500)
