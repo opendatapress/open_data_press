942436054956b64a8938ef1902ae71ed68a3df12
Craig Russell <craig@craig-russell.co.uk>
2013-08-02 16:44:39 +0100

    Added methods for fetching google user account info

--- helpers/google_api.py
+++ helpers/google_api.py
@@ -3,8 +3,10 @@
 # A wrappers and helpers for working with Google APIs
 #
 
+import httplib2
 from helpers.config import load_config, ConfigurationError
-from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import OAuth2WebServerFlow, OAuth2Credentials
+from oauth2client.anyjson import simplejson as json
 
 # An oAuth2 authentication flow handler
 def oauth2_flow():
@@ -17,3 +19,15 @@ def oauth2_flow():
             redirect_uri  = config['google_api']['redirect_uri'])
     except KeyError as e:
         raise ConfigurationError("Missing required configuration parameter %s" % e)
+
+# Get an HTTP client authorised with oAuth2 credentials json
+def http_from_oauth2(auth_json):
+    http  = httplib2.Http()
+    creds = OAuth2Credentials.from_json(auth_json)
+    return creds.authorize(http)
+
+# Get user information using credentials json
+def user_info(auth_json):
+    http = http_from_oauth2(auth_json)
+    response = http.request('https://www.googleapis.com/oauth2/v2/userinfo')
+    return json.loads(response[1])
--- tests/test_helpers/test_google_api.py
+++ tests/test_helpers/test_google_api.py
@@ -10,3 +10,9 @@ class GoogleAPITest(unittest.TestCase):
     def test_oauth2_flow(self):
         flow = google_api.oauth2_flow()
         self.assertIsInstance(flow, OAuth2WebServerFlow)
+
+    def test_http_from_oauth2(self):
+        self.assertIn('http_from_oauth2', dir(google_api))
+
+    def test_user_info(self):
+        self.assertIn('user_info', dir(google_api))
