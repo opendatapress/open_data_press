ebe2b5cf844d87ceea3a8b252fd0147e45d5479b
Craig Russell <craig@craig-russell.co.uk>
2013-08-02 13:18:58 +0100

    Loggin 404 and 500 errors

--- controllers/root.py
+++ controllers/root.py
@@ -2,7 +2,7 @@
 #
 # Base routes and error handlers
 #
-
+import logging
 from webapp2 import RequestHandler
 from helpers.views import static, render
 
@@ -15,10 +15,12 @@ class HomeRoute(RequestHandler):
 
 
 def error_404(request, response, exception):
+    logging.error("%s - %s" % (request.path, exception))
     response.write(static('404.html'))
     response.set_status(404)
 
 
 def error_500(request, response, exception):
+    logging.error("%s - %s" % (request.path, exception))
     response.write(static('500.html'))
     response.set_status(500)
