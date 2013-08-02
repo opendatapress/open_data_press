019d12c9cc71f3ffbfbc4a37836e96cae00e73da
Craig Russell <craig@craig-russell.co.uk>
2013-08-02 15:34:47 +0100

    Added authentication against Google oAuth2

--- controllers/session.py
+++ controllers/session.py
@@ -2,20 +2,34 @@
 #
 # Session route handlers
 #
-
+import logging
 from webapp2 import RequestHandler
+from helpers import google_api
+from oauth2client.client import FlowExchangeError
 
 class LoginRoute(RequestHandler):
-
     def get(self):
-        self.response.write('login')
+        flow = google_api.oauth2_flow()
+        self.redirect(flow.step1_get_authorize_url())
 
 class LogoutRoute(RequestHandler):
-
     def get(self):
         self.response.write('logout')
 
 class OAuth2CallbackRoute(RequestHandler):
-
     def get(self):
-        self.response.write('oauth2callback')
+        
+        code = self.request.GET.get('code')
+        if None == code:
+            return self.response.write('No authentication code returned')
+
+        try:
+            flow = google_api.oauth2_flow()
+            auth = flow.step2_exchange(code)
+            self.response.write('Authenticated <a href="/auth/login">re-login</a><br><code>%s</code>' % auth.to_json())
+        except FlowExchangeError as e:
+            logging.error("oAuth2 Flow Exchange Error: %s" % e)
+            self.response.write('Flow exchange error: %s' % e)
+        except Exception as e:
+            logging.error("oAuth2 Unknown Error: %s" % e)
+            self.response.write('Something went wrong: %s' % e)
--- tests/test_routes/test_session.py
+++ tests/test_routes/test_session.py
@@ -6,9 +6,11 @@ import main # The app
 
 class TestSessionHandler(unittest.TestCase):
 
-    def test_login_path_responds(self):
+    def test_login_path_redirects_to_google_auth_endpoint(self):
         response = main.app.get_response('/auth/login')
-        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.status_int, 302)
+        self.assertIn('Location', response.headers)
+        self.assertIn('accounts.google.com', response.headers['Location'])
 
     def test_logout_path_responds(self):
         response = main.app.get_response('/auth/logout')
@@ -17,3 +19,5 @@ class TestSessionHandler(unittest.TestCase):
     def test_oauth2callback_path_responds(self):
         response = main.app.get_response('/auth/oauth2callback')
         self.assertEqual(response.status_int, 200)
+
+    # TODO test oAuth2 authentication flow somehow...?
