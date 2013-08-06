2208c118dbcfaf0a469506429e8a6fbef4c7d2de
Craig Russell <craig@craig-russell.co.uk>
2013-08-06 16:57:54 +0100

    Tidied up repetative tests

--- tests/test_routes/test_api.py
+++ tests/test_routes/test_api.py
@@ -22,176 +22,90 @@ USER_AUTH_JSON="""
 
 class TestAPIHandler(unittest.TestCase):
 
-    def test_api_path_denies_unauthenticated_requests(self):
-        response = main.app.get_response('/api')
-        self.assertEqual(response.status_int, 404)
+    # Helper method to test generic error responses
+    def denied_authentication(self, response, code=404):
+        self.assertEqual(response.status_int, code)
         self.assertEqual(response.content_type, 'application/json')
         self.assertIsInstance(json.loads(response.body), dict)
         data = json.loads(response.body)
         self.assertTrue('response' in data)
         self.assertEqual('error', data['response'])
+
+
+    def test_api_path_denies_unauthenticated_requests(self):
+        response = main.app.get_response('/api')
+        self.denied_authentication(response)
 
 
     def test_api_0_path_denies_unauthenticated_requests(self):
         response = main.app.get_response('/api/0')
-        self.assertEqual(response.status_int, 404)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
+        self.denied_authentication(response)
 
 
     def test_api_0_user_path_denies_unauthenticated_requests(self):
+        
         response = main.app.get_response('/api/0/user')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/user', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
+        self.denied_authentication(response, 403)
 
 
     def test_api_0_data_source_path_denies_unauthenticated_requests(self):
+        
         response = main.app.get_response('/api/0/data_source')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source',method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1', method='DELETE')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
+        self.denied_authentication(response, 403)
 
 
     def test_api_0_data_view_path_denies_unauthenticated_requests(self):
+        
         response = main.app.get_response('/api/0/data_source/1/view')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1/view', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1/view/0')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1/view/0', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/data_source/1/view/0', method='DELETE')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
+        self.denied_authentication(response, 403)
 
 
     def test_api_0_google_sheets_path_denies_unauthenticated_requests(self):
+        
         response = main.app.get_response('/api/0/google/sheets')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/google/sheets', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/google/sheets/0')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/google/sheets/0', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/google/sheets/0/0')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
-
+        self.denied_authentication(response, 403)
+        
         response = main.app.get_response('/api/0/google/sheets/0/0', method='POST')
-        self.assertEqual(response.status_int, 403)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIsInstance(json.loads(response.body), dict)
-        data = json.loads(response.body)
-        self.assertTrue('response' in data)
-        self.assertEqual('error', data['response'])
+        self.denied_authentication(response, 403)
 
 
     def test_api_0_get_google_sheets_list(self):
