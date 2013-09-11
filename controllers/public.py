db3e709a47b1044ac6eff1fe315b6d51a62a12a1
Craig Russell <craig@craig-russell.co.uk>
2013-09-11 18:26:09 +0100

    Created basic user profile page

--- controllers/public.py
+++ controllers/public.py
@@ -3,14 +3,23 @@
 # Public site route handlers
 #
 
-from webapp2 import RequestHandler
+from models.user import User
+from helpers.views import render
+from helpers.sessions import SessionHandler
 
-class ProfileRoute(RequestHandler):
+class ProfileRoute(SessionHandler):
 
     def get(self, profile_slug):
-        self.response.write('profile')
+        user = User.get_by_slug(profile_slug)
+        if user:
+            data = {'user': user, 'current_user': user.to_dict(), 'session': self.session}
+            body = render('user_profile.html', data)
+            self.response.write(body)
+        else:
+            self.response.write('No such user')
 
-class DataSourceRoute(RequestHandler):
+
+class DataSourceRoute(SessionHandler):
 
     def get(self, profile_slug, data_source_slug):
         if 'copy' in self.request.GET.keys():
@@ -18,7 +27,7 @@ class DataSourceRoute(RequestHandler):
         else:
             self.response.write('view data source')
 
-class DataViewRoute(RequestHandler):
+class DataViewRoute(SessionHandler):
 
     def get(self, profile_slug, data_source_slug, data_view_ext):
         self.response.write('data view')
