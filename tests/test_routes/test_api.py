3a3292f9a368bb62812e708ba4d1d97c2efce16f
Craig Russell <craig@craig-russell.co.uk>
2013-10-23 16:15:06 +0100

    Tidied up google drive api tests. Using dummy key and id to test data source creation

--- tests/test_routes/test_api.py
+++ tests/test_routes/test_api.py
@@ -28,7 +28,7 @@ class TestAPIHandler(unittest.TestCase):
 
 
     # Helper method to test generic error responses
-    def denied_authentication(self, response, code=404):
+    def response_error(self, response, code=404):
         self.assertEqual(response.status_int, code)
         self.assertEqual(response.content_type, 'application/json')
         self.assertIsInstance(json.loads(response.body), dict)
@@ -52,21 +52,21 @@ class TestAPIHandler(unittest.TestCase):
 
     def test_api_path_denies_unauthenticated_requests(self):
         response = main.app.get_response('/api')
-        self.denied_authentication(response)
+        self.response_error(response)
 
 
     def test_api_0_path_denies_unauthenticated_requests(self):
         response = main.app.get_response('/api/0')
-        self.denied_authentication(response)
+        self.response_error(response)
 
 
     def test_api_0_user_path_denies_unauthenticated_requests(self):
         
         response = main.app.get_response('/api/0/user')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/user', method='POST')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
 
 
     def test_api_0_user_get_returns_user_data(self):
@@ -105,19 +105,19 @@ class TestAPIHandler(unittest.TestCase):
     def test_api_0_data_source_path_denies_unauthenticated_requests(self):
         
         response = main.app.get_response('/api/0/data_source')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source',method='POST')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1', method='POST')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1', method='DELETE')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
 
 
     def test_api_0_data_source_list_all(self):
@@ -131,7 +131,7 @@ class TestAPIHandler(unittest.TestCase):
 
 
     def test_api_0_data_source_add_item(self):
-        payload = {'key':'xxx', 'id':'xxx'}
+        payload  = {'key':'dummy_key', 'id':'dummy_id', 'title':'My New Open Data'}
         response = main.app.get_response('/api/0/data_source', headers=self.auth_headers, POST={"payload": json.dumps(payload)})
         self.response_ok(response)
         
@@ -151,6 +151,7 @@ class TestAPIHandler(unittest.TestCase):
 
         self.assertEqual(data['google_spreadsheet'], payload['key'])
         self.assertEqual(data['google_worksheet'],   payload['id'])
+        self.assertEqual(data['title'],              payload['title'])
 
 
     def test_api_0_data_source_get_item(self):
@@ -215,19 +216,19 @@ class TestAPIHandler(unittest.TestCase):
     def test_api_0_data_view_path_denies_unauthenticated_requests(self):
         
         response = main.app.get_response('/api/0/data_source/1/view')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1/view', method='POST')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1/view/0')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1/view/0', method='POST')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/data_source/1/view/0', method='DELETE')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
 
 
     def test_api_0_data_view_list_all(self):
@@ -253,102 +254,42 @@ class TestAPIHandler(unittest.TestCase):
     def test_api_0_google_sheets_path_denies_unauthenticated_requests(self):
         
         response = main.app.get_response('/api/0/google/sheets')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/google/sheets/abc123')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
         response = main.app.get_response('/api/0/google/sheets/abc123/abc123')
-        self.denied_authentication(response, 403)
+        self.response_error(response, 403)
         
 
     def test_api_0_get_google_sheets_list(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets', headers=headers)
-
-        self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets', headers=self.auth_headers)
+        self.response_ok(response)
+        # NB Respose body tested in helpers/test_google_api.py
 
     def test_api_0_get_google_sheets_worksheets(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets/dummy_key', headers=headers)
-
-        self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets/dummy_key', headers=self.auth_headers)
+        self.response_ok(response)
+        # NB Respose body tested in helpers/test_google_api.py
 
     def test_api_0_get_google_sheets_worksheets_not_found(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets/not_found', headers=headers)
-
-        self.assertEqual(response.status_int, 500)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets/not_found', headers=self.auth_headers)
+        self.response_error(response, 500)
 
     def test_api_0_get_google_sheets_worksheets_bad_format(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets/bad_format', headers=headers)
-
-        self.assertEqual(response.status_int, 500)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets/bad_format', headers=self.auth_headers)
+        self.response_error(response, 500)
 
     def test_api_0_get_google_sheets_cells(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets/dummy_key/dummy_id', headers=headers)
-
-        self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets/dummy_key/dummy_id', headers=self.auth_headers)
+        self.response_ok(response)
+        # NB Respose body tested in helpers/test_google_api.py
 
     def test_api_0_get_google_sheets_cells_not_found(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets/dummy_key/not_found', headers=headers)
-
-        self.assertEqual(response.status_int, 500)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets/dummy_key/not_found', headers=self.auth_headers)
+        self.response_error(response, 500)
 
     def test_api_0_get_google_sheets_cells_bad_format(self):
-        google_api.httplib2.Http = MockHttp
-
-        # Make authenticated request
-        response = main.app.get_response('/auth/oauth2callback?code=dummy_code')
-        headers  = {'Cookie': response.headers['Set-Cookie']}
-        response = main.app.get_response('/api/0/google/sheets/dummy_key/bad_format', headers=headers)
-
-        self.assertEqual(response.status_int, 500)
-        self.assertEqual(response.content_type, 'application/json')
-        # NB response content tested in helpers/test_google_api.py
-        self.assertIsInstance(json.loads(response.body), dict)
+        response = main.app.get_response('/api/0/google/sheets/dummy_key/bad_format', headers=self.auth_headers)
+        self.response_error(response, 500)
