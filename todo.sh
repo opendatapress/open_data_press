8c65fe4114bba5be3bcdc8a6d38c7b67e0cb64cb
Craig Russell <craig@craig-russell.co.uk>
2013-11-15 11:38:49 +0000

    Show only TODO comments form within the project

--- todo.sh
+++ todo.sh
@@ -1,2 +1,2 @@
 # Shows all TODO  comments in the codebase
-grep --recursive --line-number --exclude-dir="./lib" --exclude="todo.sh" TODO . | sed -e 's/:[ ]\+/\t/g'
+grep --recursive --line-number --exclude-dir="lib" --exclude-dir=".git" --exclude="todo.sh" --exclude="*.pyc" TODO . | sed -e 's/:[ ]\+/\t/g'
