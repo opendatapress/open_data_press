b77ff42484fb3bf75aaf4ff5d97089e764dac9ce
Craig Russell <craig@craig-russell.co.uk>
2013-11-20 12:54:09 +0000

    Removed silly todo script

--- todo.sh
+++
@@ -1,2 +0,0 @@
-# Shows all TODO  comments in the codebase
-grep --recursive --line-number --exclude-dir="lib" --exclude-dir=".git" --exclude="todo.sh" --exclude="*.pyc" TODO . | sed -e 's/:[ ]\+/\t/g'
