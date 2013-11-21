55ea60e69f7d6c4f3fe145adc1b175490cd9f1ba
Craig Russell <craig@craig-russell.co.uk>
2013-11-21 21:10:08 +0000

    Fixed broken test requiring GAE stubs

--- tests/test_routes/test_root.py
+++ tests/test_routes/test_root.py
@@ -2,9 +2,18 @@
 
 import unittest
 import webapp2
+from google.appengine.ext import testbed
 import main # The app
 
 class TestRootHandler(unittest.TestCase):
+
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
 
     def test_root_path_responds(self):
         request = webapp2.Request.blank('/')
